a6f671084c8b5ac62498306746470be2d4bf6005
Michelle Epstein <epstein.michelle@gmail.com>
2016-11-29 10:04:21 +0000

    Created Statement class and tests

---
+++ spec/statement_spec.rb
@@ -0,0 +1,11 @@
+require 'statement'
+
+describe Statement do
+
+  subject(:statement) {described_class.new}
+
+  it 'records transactions' do
+    expect(Statement).to eq(Statement)
+  end
+  
+end
