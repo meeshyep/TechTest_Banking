f2d7f4835b93dad2b0449483450992d8967eb867
Michelle Epstein <epstein.michelle@gmail.com>
2016-11-29 10:47:34 +0000

    Added Statement tests

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,26 @@
 class Statement
-  
+
+  def initialize
+    @account_history = []
+  end
+
+  def record(transaction)
+    @account_history << transaction
+  end
+
+  def to_s
+    output = "date || credit || debit || balance\n"
+    @account_history.each do |transaction|
+      output << "#{transaction[:date]} || #{credit_debit(transaction[:amount])} || #{transaction[:balance]}\n"
+    end
+
+    return output
+  end
+
+private
+
+def credit_debit(amount)
+  amount.value < 0 ? "|| #{amount}" : "#{amount} ||"
+end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,9 +3,25 @@ require 'statement'
 describe Statement do
 
   subject(:statement) {described_class.new}
+  let (:credit) {double :money, to_s: "$500.00", value: 500}
+  let (:debit) {double :money, to_s: "$500.00", value: -500}
 
-  it 'records transactions' do
-    expect(Statement).to eq(Statement)
+  it 'prints an empty statement' do
+    expect(statement.to_s).to eq("date || credit || debit || balance\n")
   end
-  
+
+  it 'records transactons' do
+    transaction = {date: Date.today, amount: credit, balance: credit }
+    expect(statement.record(transaction)).to eq([{date: Date.today, amount: credit, balance: credit}])
+  end
+
+  it 'prints credit transactions when they are entered' do
+    statement.record(date: Date.today, amount: credit, balance: credit)
+    expect(statement.to_s).to include("2016-11-29 || £500.00 || || £500.00\n")
+  end
+
+  it 'prints debit transactions when they are entered' do
+    statement.record(date: Date.today, amount: debit, balance: debit)
+    expect(statement.to_s).to include("2016-11-29 || || £500.00 || £500.00\n")
+  end
 end
